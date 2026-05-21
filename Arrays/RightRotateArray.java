be55943ffeb944b87f7487a2ad88b27118abc6f5
Ajay0603-Git <ajaygithub0603@gmail.com>
2026-05-21 13:31:48 +0530

    added rightrotatearray code

--- Arrays/RightRotateArray.java
+++ Arrays/RightRotateArray.java
@@ -1,6 +1,6 @@
 package Arrays;
 import java.util.Scanner;
-public class LeftRotateArray {
+public class RightRotateArray {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter the size of array");
@@ -13,13 +13,12 @@ public class LeftRotateArray {
         {
             arr[i]=sc.nextInt();
         }
-        int temp=arr[0];
-
-        for(int i=0;i<n-1;i++)
+        int temp=arr[n-1];
+        for(int i=n-1;i>0;i--)
         {
-         arr[i]=arr[i+1];
+            arr[i]=arr[i-1];
         }
-        arr[n-1]=temp;
+        arr[0]=temp;
         for(int i=0;i<n;i++)
         {
             System.out.println(arr[i]);
