16d5e8f926175e40cc5e4da60d65d2b162f11af8
Ajay0603-Git <ajaygithub0603@gmail.com>
2026-05-21 13:10:37 +0530

    added code for left rotate of array

---
+++ Arrays/LeftRotateArray.java
@@ -0,0 +1,32 @@
+package Arrays;
+import java.util.Scanner;
+public class LeftRotateArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the size of array");
+        int n=sc.nextInt();
+        
+        int arr[]=new int[n];
+         
+        System.out.println("enter array elements");
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }int temp=arr[0];
+
+        for(int i=0;i<n-1;i++)
+        {
+            
+            arr[i]=arr[i+1];
+            
+            
+
+        }
+        arr[n-1]=temp;
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
